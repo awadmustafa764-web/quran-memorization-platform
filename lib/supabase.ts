0f55c66390945d6e3bc499d3a60111d31da7d045
awadmustafa764-web <awadmustafa764@gmail.com>
2026-09-16 13:24:18 +0300

    Fix Supabase Keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// أنا استخرجتلك رابط مشروعك الصحيح من الصور اللي بعثتها
+const supabaseUrl = 'https://yzrlnfmofnwqsbddwudk.supabase.co'
+
+// هون لازم تحط المفتاح تبعك
+const supabaseKey = 'sb_publishable_RRbWkwa583NQZRdh0k-1Lg_zNn8mW-m'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
